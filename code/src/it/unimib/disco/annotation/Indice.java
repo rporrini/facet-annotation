5c3bf29be604f8ea07de35694bdc4a91fbfb4de4
VALE <VALE@Vale.homenet.telecomitalia.it>
2014-01-18 17:31:27 +0100

    pp

---
+++ code/src/it/unimib/disco/annotation/Indice.java
@@ -0,0 +1,112 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+
+public class Indice {
+
+	/**
+	 * @param args
+	 */
+	String indexPath = "C:/Users/VALE/Desktop/trank-indexes/pathindex";
+	String indexPathType = "C:/Users/VALE/Desktop/trank-indexes/typeindex/";
+	String indexPathLabel = "C:/Users/VALE/Desktop/trank-indexes/uriindex/";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void interrogaIndice(Vector vettURL) throws CorruptIndexException, IOException, ParseException {
+		// TODO Auto-generated method stub
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
+		//apertura indice path
+		Directory index = FSDirectory.open(new File(indexPath));
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		//apertura indice Type
+		Directory indexType = FSDirectory.open(new File(indexPathType));
+		IndexReader readerType = DirectoryReader.open(indexType);
+		IndexSearcher searcherType = new IndexSearcher(readerType);
+		//apertura indice label
+	/*	Directory indexLabel = FSDirectory.open(new File(indexPathLabel));
+		IndexReader readerLabel = DirectoryReader.open(indexLabel);
+		IndexSearcher searcherLabel = new IndexSearcher(readerLabel);
+	*/	
+		String querystr1 = "uri:http***dbpedia*org*class*yago*Demons";
+		Query q = new QueryParser(Version.LUCENE_CURRENT, "uri", analyzer).parse(querystr1);
+		//risultati
+		TopScoreDocCollector collector = TopScoreDocCollector.create(10, true);
+		searcher.search(q, collector);
+		ScoreDoc[] docs = collector.topDocs().scoreDocs;
+		for (int i = 0; i < docs.length; i++) {
+		  Document result = searcher.doc(docs[i].doc);
+		  System.out.println(result);
+		}
+		WordUrls url = (WordUrls) vettURL.elementAt(0);
+		String uri = url.getVettUrl().elementAt(0);
+		uri = uri.replaceAll(":", "*");
+		uri = uri.replaceFirst("\\.", "*");
+		uri = uri.replaceAll("/", "*");
+		//query
+		String querystr = "uri:"+uri;
+		 querystr = "uri:http***dbpedia*org*class*yago*Demons";
+		//String querystr = "uri:\"http://dbpedia.org/resource/Nature\"";
+		//String querystr = "uri:http***dbpedia*org*resource*nature";
+		//query
+	/*	Vector URIsTipi = new Vector();
+				Query qType = new QueryParser(Version.LUCENE_42, "uri", analyzer).parse(querystr);
+				//risultati
+				TopScoreDocCollector collectorType = TopScoreDocCollector.create(10, true);
+				searcherType.search(qType, collectorType);
+				ScoreDoc[] docsType = collectorType.topDocs().scoreDocs;
+				for (int i = 0; i < docsType.length; i++) {
+					Document result = searcherType.doc(docsType[i].doc);
+					List<IndexableField> listaTipi = result.getFields();
+					for (int j=0; j< listaTipi.size(); j++){
+						String URItipo = (listaTipi.get(j)).stringValue();
+						URIsTipi.add(URItipo);
+						//System.out.println(result);
+					}
+				}
+				
+				String uri1 = (String) URIsTipi.elementAt(0);
+				uri1 = uri1.replaceAll(":", "*");
+				uri1 = uri1.replaceFirst("\\.", "*");
+				uri1 = uri1.replaceAll("/", "*");
+				//query
+				// String querystr1 = "uri:"+uri;
+	/*			String querystr1 = "uri:http***dbpedia*org*class*yago*Demons";
+		Query q = new QueryParser(Version.LUCENE_42, "path", analyzer).parse(querystr1);
+		//risultati
+		TopScoreDocCollector collector = TopScoreDocCollector.create(10, true);
+		searcher.search(q, collector);
+		ScoreDoc[] docs = collector.topDocs().scoreDocs;
+		for (int i = 0; i < docs.length; i++) {
+		  Document result = searcher.doc(docs[i].doc);
+		  System.out.println(result);
+		}
+		*/
+		
+		
+	
+	}
+}
---
+++ code/src/it/unimib/disco/annotation/Trank.java
@@ -0,0 +1,119 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+
+/*import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+*/
+import net.sf.json.*;
+
+public class Trank {
+
+	/**
+	 * @param args
+	 */
+	private final String USER_AGENT = "Mozilla/25.0.1";
+	String credenziali = "app_id=bbff1282&app_key=bfb47741212ddde1d0d56eacc0141512";
+	String lang = "lang=it";
+	static int contRiga = 0;
+	public static String pathCartellaGroupsEvaluation = "C:/Users/Vale/Desktop/Magistrale Secondo Anno/Intelligenza Artificiale/pratica/evaluation/group";
+	public static String pathEtichettaturaGR = "C:/Users/Vale/Desktop/Magistrale Secondo Anno/Intelligenza Artificiale/pratica/evaluation/";
+	public static String pathfileGRXML = "C:/Users/Vale/Desktop/Magistrale Secondo Anno/Intelligenza Artificiale/pratica/evaluation/";
+	
+	public static String pathGroups = "C:/Users/VALE/evaluation/group/";
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		// TODO Auto-generated method stub
+		Trank http = new Trank();
+		Indice indLucene = new Indice();
+		
+		String indexPath = "C:/Users/VALE/Desktop/trank-indexes/pathindex";
+		String indexPathType = "C:/Users/VALE/Desktop/trank-indexes/typeindex/";
+		String indexPathLabel = "C:/Users/VALE/Desktop/trank-indexes/uriindex/";
+		
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
+		//apertura indice path
+		Directory index = FSDirectory.open(new File(indexPath));
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		//apertura indice Type
+		Directory indexType = FSDirectory.open(new File(indexPathType));
+		IndexReader readerType = DirectoryReader.open(indexType);
+		IndexSearcher searcherType = new IndexSearcher(readerType);
+		
+		//con machine linking il primo riferimento che ottengo a DBpedia è questa URI
+		String uri = "uri:http***dbpedia*org*resource*Nature";
+		Vector URIsTipi = new Vector();
+		Query qType = new QueryParser(Version.LUCENE_42, "uri", analyzer).parse(uri);
+		//risultati
+		TopScoreDocCollector collectorType = TopScoreDocCollector.create(10, true);
+		searcherType.search(qType, collectorType);
+		ScoreDoc[] docsType = collectorType.topDocs().scoreDocs;
+		for (int i = 0; i < docsType.length; i++) {
+			Document result = searcherType.doc(docsType[i].doc);
+			List<IndexableField> listaTipi = result.getFields();
+			for (int j=0; j< listaTipi.size(); j++){
+				String URItipo = (listaTipi.get(j)).stringValue();
+				URIsTipi.add(URItipo);
+				System.out.println(result);
+			}
+		}
+		
+		//esempio query perchè nella fase prima http://dbpedia/org/class/yago/Demons è il primo tipo che recupero
+		//se cerco la stessa identica stringa con luke funziona qui no
+		String querystr1 = "uri:http***dbpedia*org*class*yago*Demons";
+		
+		Directory directory = FSDirectory.open(new File("C:/Users/VALE/Desktop/trank-indexes/pathindex"));
+		IndexReader indexReader = IndexReader.open(directory);
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+		analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
+		QueryParser queryParser = new QueryParser(Version.LUCENE_CURRENT, "uri", analyzer);
+		Query query = queryParser.parse(querystr1);
+		TopDocs hits = indexSearcher.search(query, 10);
+		System.out.println("Number of hits: " + hits.totalHits);
+
+		
+		
+		
+		  }
+	}
+
+	
+
+
