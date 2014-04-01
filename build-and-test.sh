217afd8c43be164882534c3b59133820bb90726a
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-01 14:42:16 +0200

    semplificata procedura di build. adesso non compiliamo tutto ma solo le libraries

--- build-and-test.sh
+++ build-and-test.sh
@@ -11,16 +11,11 @@ rm -rf $build
 mkdir -p $build
 javac -cp .:'lib/*' $(find ./* | grep '\.java') -d $build
 cd $build
-for file in $(find ../../lib/* | grep jar)
-do
-	jar xf $file
-done
-rm -r META-INF
 jar cvfe ../../labelling.jar -C . > /dev/null
 chmod 777 ../../labelling.jar
 echo "******* Done *******"
 
 echo "******* Running Tests *******"
 cd $project
-java -cp .:'labelling.jar' org.junit.runner.JUnitCore it.disco.unimib.test.TestSuite
+java -cp .:'labelling.jar':'lib/*' org.junit.runner.JUnitCore it.disco.unimib.test.TestSuite
 echo "******* Done *******"
