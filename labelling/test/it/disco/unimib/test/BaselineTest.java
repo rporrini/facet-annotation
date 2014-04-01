043543d10fe5317406a80257e676d6b24a3b1d35
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-01 15:12:50 +0200

    piccolo cambiamento

--- labelling/test/it/disco/unimib/test/BaselineTest.java
+++ labelling/test/it/disco/unimib/test/BaselineTest.java
@@ -1,11 +1,12 @@
 package it.disco.unimib.test;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import io.mem0r1es.trank.TRanker;
+import io.mem0r1es.trank.ranking.ANCESTORS;
 
 import java.net.URI;
-
-import io.mem0r1es.trank.TRanker;
 
 import org.junit.Test;
 
@@ -16,7 +17,7 @@ public class BaselineTest {
 
 	@Test
 	public void theBaselineShouldRun() {
-		Map<URI, Seq<URI>> types = new TRanker("University of Fribourg").entityToTRankedTypes();
+		Map<URI, Seq<URI>> types = new TRanker("University of Fribourg", new ANCESTORS()).entityToTRankedTypes();
 		
 		assertThat(types.size(), is(greaterThan(0)));
 	}
