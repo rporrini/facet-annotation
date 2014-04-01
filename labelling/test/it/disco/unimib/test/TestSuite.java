ad42bff1fdce2450c4633d63411ed07c02296d31
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-01 14:27:44 +0200

    aggiunto stub per i test

---
+++ labelling/test/it/disco/unimib/test/TestSuite.java
@@ -0,0 +1,11 @@
+package it.disco.unimib.test;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.extensions.cpsuite.ClasspathSuite.ClassnameFilters;
+import org.junit.extensions.cpsuite.ClasspathSuite.IncludeJars;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@IncludeJars(true)
+@ClassnameFilters({".*Test"})
+public class TestSuite {}
---
+++ labelling/test/it/disco/unimib/test/TrivialTest.java
@@ -0,0 +1,13 @@
+package it.disco.unimib.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TrivialTest {
+	
+	@Test
+	public void aTrivialTestShouldBeExecutedAndPass() throws Exception {
+		assertTrue(true);
+	}
+}
