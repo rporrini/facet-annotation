be9cb225448e99baec9a83debdebb82aa42ea43b
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-01 19:39:32 +0200

    adattato trank al nostro caso

--- labelling/baseline/it/disco/unimib/baseline/TRankBaseline.java
+++ labelling/baseline/it/disco/unimib/baseline/TRankBaseline.java
@@ -7,6 +7,7 @@ import io.mem0r1es.trank.ranking.ANCESTORS;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,22 +19,37 @@ import com.typesafe.config.ConfigFactory;
 
 public class TRankBaseline {
 
-	public List<String> typesOf(String... entity) throws URISyntaxException {
+	public String typeOf(String... entity) throws URISyntaxException {
 		List<URI> entities = new ArrayList<URI>();
 		for(String e : entity){
 			entities.add(new URI(e));
 		}
-		Map<URI, Seq<URI>> types = rankTypes(entities);
-		List<String> retrievedTypes = new ArrayList<String>();
-		for(URI uri : types.keySet()){
-			for(URI type: JavaConversions.asJavaCollection(types.get(uri))){
-				retrievedTypes.add(type.toString());
+		HashMap<URI, Double> scores = evaluateScoresOf(rankedTypes(entities));
+		double maxScore = -1d;
+		URI candidate = null;
+		for(URI type : scores.keySet()){
+			double score = scores.get(type);
+			if(score > maxScore){
+				candidate = type;
+				maxScore = score;
 			}
 		}
-		return retrievedTypes;
+		return candidate.toString();
 	}
 
-	private Map<URI, Seq<URI>> rankTypes(List<URI> entities) {
+	private HashMap<URI, Double> evaluateScoresOf(Map<URI, Seq<URI>> types) {
+		HashMap<URI, Double> scores = new HashMap<URI, Double>();
+		for(URI uri : types.keySet()){
+			List<URI> currentTypes = JavaConversions.asJavaList(types.get(uri));
+			for(URI type: currentTypes){
+				if(!scores.containsKey(type)) scores.put(type, 0d);
+				scores.put(type, scores.get(type) + (1d / Math.sqrt((double)currentTypes.indexOf(type))));
+			}
+		}
+		return scores;
+	}
+
+	private Map<URI, Seq<URI>> rankedTypes(List<URI> entities) {
 		scala.collection.immutable.Set<URI> set = JavaConversions.asScalaBuffer(entities).toSet();
 		Config config = ConfigFactory.load();
 		return JavaConversions.asJavaMap(TypeRanking.rankTypes(TypeRetrieval.retrieveTypes(set, config), new ANCESTORS(), config));
--- labelling/test/it/disco/unimib/test/BaselineTest.java
+++ labelling/test/it/disco/unimib/test/BaselineTest.java
@@ -1,11 +1,8 @@
 package it.disco.unimib.test;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 import it.disco.unimib.baseline.TRankBaseline;
-
-import java.util.List;
 
 import org.junit.Test;
 
@@ -15,8 +12,8 @@ public class BaselineTest {
 	public void tRankShouldGiveASetOfTypesGivenAnEntity() throws Exception {
 		TRankBaseline baseline = new TRankBaseline();
 		
-		List<String> types = baseline.typesOf("http://dbpedia.org/resource/Tom_Hanks", "http://dbpedia.org/resource/Harrison_Ford");
+		String type = baseline.typeOf("http://dbpedia.org/resource/Tom_Hanks", "http://dbpedia.org/resource/Harrison_Ford");
 		
-		assertThat(types, hasSize(greaterThan(1)));
+		assertThat(type, containsString("AmericanFilmActors"));
 	}
 }
