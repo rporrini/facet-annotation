1fedbb3eb05f12d2ec95ee7bd989e65aaaafe667
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-14 18:25:47 +0200

    messo a posto un po di nomi

---
+++ key-value-index-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function signal(){
+	echo "******* $1 *******"
+}
+
+set -e
+relative_path=`dirname $0`
+root=`cd $relative_path;pwd`
+cd $root
+./key-value-index.sh geonames geonames/types "http://www.geonames.org/ontology#featureCode"
+./key-value-index.sh geonames-ontologies geonames/labels 'http://www.w3.org/2004/02/skos/core#prefLabel'
+./key-value-index.sh linkedbrainz linkedbrainz/types "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+./key-value-index.sh linkedbrainz-ontologies linkedbrainz/labels "http://www.w3.org/2000/01/rdf-schema#label"
+
