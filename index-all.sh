0b8783d6ae6ecdb96fbbcd8dd7a84d732af4fd72
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-16 09:55:24 +0200

    aggiunta anche creazione dell'indice delle property

--- index-all.sh
+++ index-all.sh
@@ -10,6 +10,9 @@ root=`cd $relative_path;pwd`
 cd $root
 ./key-value-index.sh geonames geonames/types "http://www.geonames.org/ontology#featureCode"
 ./key-value-index.sh geonames-ontologies geonames/labels 'http://www.w3.org/2004/02/skos/core#prefLabel'
+./predicate-value-index.sh geonames
+
 ./key-value-index.sh linkedbrainz linkedbrainz/types "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 ./key-value-index.sh linkedbrainz-ontologies linkedbrainz/labels "http://www.w3.org/2000/01/rdf-schema#label"
+./predicate-value-index.sh linkedbrainz
 
