1f6e23b0dfc2335fbd0ff371d56571b39340d194
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-17 10:08:44 +0200

    aggiunta indicizzazione di dbpedia

--- index-all-on-separate-indexes.sh
+++ index-all-on-separate-indexes.sh
@@ -10,9 +10,13 @@ root=`cd $relative_path;pwd`
 cd $root
 ./key-value-index.sh geonames geonames/types "http://www.geonames.org/ontology#featureCode"
 ./key-value-index.sh geonames-ontologies geonames/labels 'http://www.w3.org/2004/02/skos/core#prefLabel'
-./predicate-value-index.sh geonames
+./predicate-value-index.sh geonames geonames
 
 ./key-value-index.sh linkedbrainz linkedbrainz/types "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 ./key-value-index.sh linkedbrainz-ontologies linkedbrainz/labels "http://www.w3.org/2000/01/rdf-schema#label"
-./predicate-value-index.sh linkedbrainz
+./predicate-value-index.sh linkedbrainz linkedbrainz
+
+./key-value-index.sh dbpedia-types dbpedia/types "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+./key-value-index.sh dbpedia-ontologies dbpedia/labels "http://www.w3.org/2000/01/rdf-schema#label"
+./predicate-value-index.sh dbpedia dbpedia-properties
 
