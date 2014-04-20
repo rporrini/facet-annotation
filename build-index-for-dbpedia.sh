f06cee11fa801da4c6579cc886d246c36cc8214b
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-20 12:20:54 +0200

    aggiunta anche indicizzazione con il contesto

--- build-index-for-dbpedia.sh
+++ build-index-for-dbpedia.sh
@@ -12,3 +12,5 @@ cd $root
 ./key-value-index.sh dbpedia-types dbpedia/types "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 ./key-value-index.sh dbpedia-categories dbpedia/types "http://purl.org/dc/terms/subject"
 ./key-value-index.sh dbpedia-labels dbpedia/labels "http://www.w3.org/2000/01/rdf-schema#label"
+./predicate-value-index.sh dbpedia-properties dbpedia/properties dbpedia/types dbpedia/labels
+
