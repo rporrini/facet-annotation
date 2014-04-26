2feb123f17390f20e609ff9c3ba3e651d1c650c3
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-26 10:30:36 +0200

    aaa

--- labelling/core/it/disco/unimib/labeller/index/Try.java
+++ labelling/core/it/disco/unimib/labeller/index/Try.java
@@ -10,8 +10,8 @@ import java.util.List;
 public class Try {
 
 	public static void main(String[] args) throws Exception {
-		String file = "tv series_wikipedia_television shows_director_List_of_How_I_Met_Your_Mother_episodes";
-		GoldStandardGroup group = new GoldStandardGroup(new FileSystemConnector(new File("../evaluation/gold-standard/" + file)));
+		String file = "allstartnba_basketball players_draft year_players-by-draft-pick.htm";
+		GoldStandardGroup group = new GoldStandardGroup(new FileSystemConnector(new File("../evaluation/gold-standard-enhanced/" + file)));
 		
 		RankingStrategy ranking = new RankByFrequency();
 		AnnotationAlgorithm maximumLikelihood = new BenchmarkConfiguration("maximum likelihood").predicateAnnotation(new RankInspection(ranking), new MandatoryContext()).getAlgorithm();
