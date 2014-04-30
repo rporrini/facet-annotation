f2a22626891bd5f8005a2a4d78c91f0f92a95f3d
rporrini <riccardo.porrini@disco.unimib.it>
2014-04-30 14:37:57 +0200

    estratta logica per la gestione dei predicati rdf

--- labelling/core/it/disco/unimib/labeller/index/AnnotationResult.java
+++ labelling/core/it/disco/unimib/labeller/index/AnnotationResult.java
@@ -1,23 +1,22 @@
 package it.disco.unimib.labeller.index;
 
+
 public class AnnotationResult implements Comparable<AnnotationResult>{
 
-	private String value;
-	private String label;
 	private double score;
+	private RDFPredicate predicate;
 
 	public AnnotationResult(String value, double score) {
-		this.value = value;
-		this.label = getLabel(value);
 		this.score = score;
+		this.predicate = new RDFPredicate(value);
 	}
 
 	public String value() {
-		return value;
+		return predicate.uri();
 	}
 	
 	public String label() {
-		return label;
+		return predicate.label();
 	}
 	
 	public double score(){
@@ -26,19 +25,11 @@ public class AnnotationResult implements Comparable<AnnotationResult>{
 	
 	@Override
 	public String toString() {
-		return value + " [" + score + "]";
+		return value() + " [" + score + "]";
 	}
 
 	@Override
 	public int compareTo(AnnotationResult other) {
 		return (int) Math.signum(other.score() - this.score());
-	}
-	
-	private String getLabel(String value) {
-		String label = value;
-		for (int index = value.indexOf('/'); index >= 0; index = value.indexOf('/', index + 1)){
-			   label = value.substring(index+1);
-		}
-		return label;
 	}
 }
---
+++ labelling/core/it/disco/unimib/labeller/index/RDFPredicate.java
@@ -0,0 +1,20 @@
+package it.disco.unimib.labeller.index;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class RDFPredicate {
+	private String uri;
+
+	public RDFPredicate(String uri){
+		this.uri = uri;
+	}
+	
+	public String label() {
+		String[] splitted = StringUtils.split(uri, "/#");
+		return splitted[splitted.length - 1];
+	}
+	
+	public String uri(){
+		return this.uri;
+	}
+}
