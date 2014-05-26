7d198d4e8a7b56b2215dcef6a95cf7eab5d6e62b
rporrini <riccardo.porrini@disco.unimib.it>
2014-05-26 18:49:40 +0200

    aggiunto script per la generazione dei giudizi di rilevanza

---
+++ labelling/benchmark/it/disco/unimib/labeller/benchmark/GetGoldStandardQRels.java
@@ -0,0 +1,37 @@
+package it.disco.unimib.labeller.benchmark;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.jopendocument.dom.spreadsheet.Sheet;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
+
+public class GetGoldStandardQRels {
+
+	public static void main(String[] args) throws Exception {
+		String questionnaire = "../evaluation/results/majority-05-all-withcontext-enhanced-withlabels-ALL.ods";
+		
+		SpreadSheet document = SpreadSheet.createFromFile(new File(questionnaire));
+		Sheet resultSheet = document.getSheet(document.getSheetCount() - 1);
+		
+		List<String> rows = new ArrayList<String>();
+		for(int row=1; row<=7151; row++){
+			try{
+				Long id = Long.parseLong(resultSheet.getCellAt("A" + row).getTextValue());
+				String content = resultSheet.getCellAt("A" + row).getTextValue();
+				while(!content.isEmpty()){
+					if(content.startsWith("http://")){
+						double rel = Double.parseDouble(resultSheet.getCellAt("C" + row).getTextValue());
+						rows.add(id + " Q0 " + content + " " + Math.round(rel));
+					}
+					row++;
+					content = resultSheet.getCellAt("A" + row).getTextValue();
+				}
+			}
+			catch(Exception e){}
+		}
+		FileUtils.writeLines(new File("../evaluation/results/gold-standard.qrels"), rows);
+	}
+}
