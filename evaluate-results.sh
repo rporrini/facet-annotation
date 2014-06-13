3e70cc9e2fbac7e8140dba8f352bf544b4d7f535
Brando Preda <brandopreda@nb-preda.(none)>
2014-06-13 16:46:45 +0200

    modificato lo script di valutazione dei risultati perchè considerava come rilevanti solo i valori con score superiore a 3

--- evaluate-results.sh
+++ evaluate-results.sh
@@ -6,7 +6,7 @@ run=$2
 
 function run-trec-eval
 {
-	trec_eval -l3 -M 20 -m runid -m map -m iprec_at_recall -m ndcg_cut.01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20 -m num_ret -m num_q -m num_rel -m num_rel_ret "$@" | sed 's/_1 /_01 /g;s/_2 /_02 /g;s/_3 /_03 /g;s/_4 /_04 /g;s/_5 /_05 /g;s/_6 /_06 /g;s/_7 /_07 /g;s/_8 /_08 /g;s/_9 /_09 /g'
+	trec_eval -M 20 -m runid -m map -m iprec_at_recall -m ndcg_cut.01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20 -m num_ret -m num_q -m num_rel -m num_rel_ret "$@" | sed 's/_1 /_01 /g;s/_2 /_02 /g;s/_3 /_03 /g;s/_4 /_04 /g;s/_5 /_05 /g;s/_6 /_06 /g;s/_7 /_07 /g;s/_8 /_08 /g;s/_9 /_09 /g'
 }
 
 run-trec-eval $1 $2
