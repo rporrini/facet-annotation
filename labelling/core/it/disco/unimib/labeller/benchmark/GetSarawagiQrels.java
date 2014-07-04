7ce438a1855cc01157fb9eaa5767a0f71428db72
Brando Preda <brandopreda@nb-preda.(none)>
2014-07-04 17:12:26 +0200

    forzato l'ordinamento dei gruppi nella generazione dei qrels sarawagi

--- labelling/core/it/disco/unimib/labeller/benchmark/GetSarawagiQrels.java
+++ labelling/core/it/disco/unimib/labeller/benchmark/GetSarawagiQrels.java
@@ -6,7 +6,7 @@ public class GetSarawagiQrels {
 
 	public static void main(String[] args) {
 		String path = args[0];
-		for(GoldStandardGroup group : new UnorderedGroups(new File(path)).getGroups()){
+		for(GoldStandardGroup group : new OrderedGroups(new UnorderedGroups(new File(path))).getGroups()){
 			System.out.println(group.id() + " Q0 " + group.label() + " 1");
 		}
 	}
