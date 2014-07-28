a1cefd6324b52c58128474e34ba645bba5dfbdc9
rporrini <riccardo.porrini@disco.unimib.it>
2014-07-28 15:33:05 +0200

    analisi dei risultati

--- run-all-algorithms.sh
+++ run-all-algorithms.sh
@@ -24,20 +24,20 @@ cd $root
 dataset=$1
 
 #run mh simple no $dataset
-run mh simple partial $dataset
-run mh simple "complete" $dataset
+#run mh simple partial $dataset
+#run mh simple "complete" $dataset
 #run mh contextualized no $dataset
-run mh contextualized partial $dataset
-run mh contextualized "complete" $dataset
-#run mhw simple no $dataset
-run mhw simple partial $dataset
-run mhw simple "complete" $dataset
-#run mhw contextualized no $dataset
-run mhw contextualized partial $dataset
-run mhw contextualized "complete" $dataset
+#run mh contextualized partial $dataset
+#run mh contextualized "complete" $dataset
+run mhw simple no $dataset
+#run mhw simple partial $dataset
+#run mhw simple "complete" $dataset
+run mhw contextualized no $dataset
+#run mhw contextualized partial $dataset
+#run mhw contextualized "complete" $dataset
 #run ml simple no $dataset
-run ml simple partial $dataset
-run ml simple "complete" $dataset
+#run ml simple partial $dataset
+#run ml simple "complete" $dataset
 #run ml contextualized no $dataset
-run ml contextualized partial $dataset
-run ml contextualized "complete" $dataset
+#run ml contextualized partial $dataset
+#run ml contextualized "complete" $dataset
