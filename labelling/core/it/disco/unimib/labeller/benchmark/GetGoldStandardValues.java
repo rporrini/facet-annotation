ffd0505b37ac81bb70617c52cd875f586190eddc
rporrini <riccardo.porrini@disco.unimib.it>
2014-08-04 15:17:23 +0200

    piccolo refactoring

--- labelling/core/it/disco/unimib/labeller/benchmark/GetGoldStandardValues.java
+++ labelling/core/it/disco/unimib/labeller/benchmark/GetGoldStandardValues.java
@@ -3,48 +3,40 @@ package it.disco.unimib.labeller.benchmark;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class GetGoldStandardValues {
 
 	public static void main(String[] args) throws Exception {
 		
-		ArrayList<String> contexts = new ArrayList<String>();
-		ArrayList<String> providers = new ArrayList<String>();
-		ArrayList<String> labels = new ArrayList<String>();
+		HashMap<String, Integer> contexts = new HashMap<String, Integer>();
+		HashMap<String, Integer> providers = new HashMap<String, Integer>();
+		HashMap<String, Integer> labels = new HashMap<String, Integer>();
 		for(GoldStandardGroup goldStandard: goldStandard()){
 			addContent(contexts, goldStandard.context());
 			addContent(providers, goldStandard.provider());
 			addContent(labels, goldStandard.label());
 		}
 	
-		printAll(providers, "Providers:");
-		printAll(contexts, "Contexts:");
-		printAll(labels, "Labels:");
-		
+		printAll(providers, "Sources:");
+		printAll(contexts, "Category Labels:");
+		printAll(labels, "Predicates:");
 	}
 
-	private static void printAll(ArrayList<String> list, String label) {
-		Collections.sort(list);
-		System.out.println(label + "(" + list.size() + ")");
-		for(int i = 0; i < list.size(); i++){
-			System.out.print("	" + list.get(i));
+	private static void printAll(HashMap<String, Integer> list, String label) {
+		ArrayList<String> keys = new ArrayList<String>(list.keySet());
+		Collections.sort(keys);
+		System.out.println(label + "(" + keys.size() + ")");
+		for(String key : keys){
+			System.out.print("\t" + key + " (" + list.get(key) + ")");
 			System.out.println();
 		}
 		System.out.println();
 	}
 
-	private static void addContent(ArrayList<String> list, String content) {
-		if(!find(list, content))
-			list.add(content);
-	}
-
-	private static boolean find(ArrayList<String> list, String value){
-		boolean b = false;
-		for(int i = 0; i < list.size(); i++){
-			if(list.get(i).equalsIgnoreCase(value))
-				b = true;
-		}
-		return b;
+	private static void addContent(HashMap<String, Integer> list, String content) {
+		if(!list.containsKey(content)) list.put(content, 0);
+		list.put(content, list.get(content)+1);
 	}
 	
 	private static GoldStandardGroup[] goldStandard() {
