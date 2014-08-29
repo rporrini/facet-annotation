79bcc841e925322e81072c73a371cf23e1cb0dfa
rporrini <riccardo.porrini@disco.unimib.it>
2014-08-29 11:08:19 -0500

    stub per la generazione del corpus basato su yago

--- build-corpus-for-yago1.sh
+++ build-corpus-for-yago1.sh
@@ -5,5 +5,5 @@ relative_path=`dirname $0`
 root=`cd $relative_path;pwd`
 cd $root
 
-./corpus.sh yago1-properties yago1 yago1/types yago1/labels 2
+./corpus.sh yago1-properties yago1 yago1/types yago1/labels-english 2
 
