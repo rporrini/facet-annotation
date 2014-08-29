b9c96ba8165796c6efd62b95582e311033df459d
rporrini <riccardo.porrini@disco.unimib.it>
2014-08-29 12:33:29 -0500

    integrato script per generare yago

--- build-corpus-for-yago1.sh
+++ build-corpus-for-yago1.sh
@@ -5,5 +5,11 @@ relative_path=`dirname $0`
 root=`cd $relative_path;pwd`
 cd $root
 
-./corpus.sh yago1-properties yago1 yago1/types yago1/labels-english 2
+destination=yago1
+
+./corpus.sh yago1-properties $destination yago1/types yago1/labels-english 2
+
+cd evaluation/labeller-corpus/$destination
+cat * >> yago1-triples
+ls * | grep -v yago1-triples | xargs rm -rf
 
