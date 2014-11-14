62e5603684f330c784251b5b94ae57ccac7740cb
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-14 15:46:09 -0600

    simplified the code for the calculations of the frequencies within the algorithm

--- labelling/core/it/disco/unimib/labeller/predicates/WeightedFrequencyCoverageAndSpecificity.java
+++ labelling/core/it/disco/unimib/labeller/predicates/WeightedFrequencyCoverageAndSpecificity.java
@@ -23,31 +23,33 @@ public class WeightedFrequencyCoverageAndSpecificity implements AnnotationAlgori
 	}
 
 	@Override
-	public List<CandidatePredicate> typeOf(String context, List<String> elements) throws Exception {
+	public List<CandidatePredicate> typeOf(String domain, List<String> elements) throws Exception {
 		
 		Distribution distribution = new CandidatePredicatesReport(new CandidatePredicates(index))
-										.forValues(context, elements.toArray(new String[elements.size()]), selection);
+										.forValues(domain, elements.toArray(new String[elements.size()]), selection);
 		
 		HashMap<String, Double> frequenciesOverValues = new HashMap<String, Double>();
 		HashMap<String, Long> overallFrequencies = new HashMap<String, Long>();
 		HashMap<String, Double> coveredValues = new HashMap<String, Double>();
 		
-		for(String value : distribution.values()){
-			for(String predicate : distribution.predicates()){
-				if(!frequenciesOverValues.containsKey(predicate)) frequenciesOverValues.put(predicate, 0.0);
-				if(!overallFrequencies.containsKey(predicate)) overallFrequencies.put(predicate, index.countPredicatesInContext(predicate, context, new NoContext()));
-				
+		for(String predicate : distribution.predicates()){
+			double frequencyOverValues = 0;
+			long overallFrequency = 0;
+			double covered = 0;
+			for(String value : distribution.values()){
 				double score = distribution.scoreOf(predicate, value);
-				frequenciesOverValues.put(predicate, frequenciesOverValues.get(predicate) + score);
-				
-				if(!coveredValues.containsKey(predicate)) coveredValues.put(predicate, 0.0);
-				if(score > 0) coveredValues.put(predicate, coveredValues.get(predicate) + 1.0);
+				if(score > 0) covered++;
+				frequencyOverValues += score;
+				overallFrequency += index.countPredicatesInContext(predicate, domain, new NoContext());
 			}
+			frequenciesOverValues.put(predicate, frequencyOverValues);
+			overallFrequencies.put(predicate, overallFrequency);
+			coveredValues.put(predicate, covered);
 		}
 		
 		ArrayList<CandidatePredicate> results = new ArrayList<CandidatePredicate>();
 		for(String predicate : frequenciesOverValues.keySet()){
-			double disc = predicateSpecificity.of(predicate, context, overallFrequencies.get(predicate));
+			double disc = predicateSpecificity.of(predicate, domain, overallFrequencies.get(predicate));
 			double smoothedWFreq = Math.log(frequenciesOverValues.get(predicate) + 1.000000001);
 			double coverage = coveredValues.get(predicate) / (double)distribution.values().size();
 			double pfd = smoothedWFreq * coverage * disc;
