73ce722b5ddc4dc169e9a58354f265068ec9781b
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-14 18:16:06 -0600

    small refactoring to the algorithm

--- labelling/core/it/disco/unimib/labeller/predicates/WeightedFrequencyCoverageAndSpecificity.java
+++ labelling/core/it/disco/unimib/labeller/predicates/WeightedFrequencyCoverageAndSpecificity.java
@@ -7,7 +7,6 @@ import it.disco.unimib.labeller.index.SelectionCriterion;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 public class WeightedFrequencyCoverageAndSpecificity implements AnnotationAlgorithm{
@@ -28,9 +27,7 @@ public class WeightedFrequencyCoverageAndSpecificity implements AnnotationAlgori
 		Distribution distribution = new CandidatePredicatesReport(new CandidatePredicates(index))
 										.forValues(domain, elements.toArray(new String[elements.size()]), selection);
 		
-		HashMap<String, Double> frequenciesOverValues = new HashMap<String, Double>();
-		HashMap<String, Double> coveredValues = new HashMap<String, Double>();
-		
+		ArrayList<CandidatePredicate> results = new ArrayList<CandidatePredicate>();
 		for(String predicate : distribution.predicates()){
 			double frequencyOverValues = 0;
 			double covered = 0;
@@ -39,16 +36,11 @@ public class WeightedFrequencyCoverageAndSpecificity implements AnnotationAlgori
 				if(score > 0) covered++;
 				frequencyOverValues += score;
 			}
-			frequenciesOverValues.put(predicate, frequencyOverValues);
-			coveredValues.put(predicate, covered);
-		}
-		
-		ArrayList<CandidatePredicate> results = new ArrayList<CandidatePredicate>();
-		for(String predicate : frequenciesOverValues.keySet()){
+						
 			long overallFrequency = index.countPredicatesInContext(predicate, domain, new NoContext());
 			double disc = predicateSpecificity.of(predicate, domain, overallFrequency);
-			double smoothedWFreq = Math.log(frequenciesOverValues.get(predicate) + 1.000000001);
-			double coverage = coveredValues.get(predicate) / (double)distribution.values().size();
+			double smoothedWFreq = Math.log(frequencyOverValues + 1.000000001);
+			double coverage = covered / (double)distribution.values().size();
 			double pfd = smoothedWFreq * coverage * disc;
 			results.add(new CandidatePredicate(predicate, smoothedWFreq, coverage, disc, pfd));
 		}
