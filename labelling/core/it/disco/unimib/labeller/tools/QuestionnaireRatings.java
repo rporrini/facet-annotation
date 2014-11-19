1fd69ddd6a4bb9fd1929b4a0c028bf359b0a4fd6
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-19 13:08:52 -0600

    optimized the construction of the index

--- labelling/core/it/disco/unimib/labeller/tools/QuestionnaireRatings.java
+++ labelling/core/it/disco/unimib/labeller/tools/QuestionnaireRatings.java
@@ -18,23 +18,28 @@ class QuestionnaireRatings{
 		List<String> rows = new ArrayList<String>();
 		for(int row=1; row<=7151; row++){
 			try{
-				Long id = Long.parseLong(resultSheet.getCellAt("A" + row).getTextValue());
-				String content = resultSheet.getCellAt("A" + row).getTextValue();
-				while(!content.equals("DBPEDIA PROPERTY")){
-					row++;
-					content = resultSheet.getCellAt("A" + row).getTextValue();
-				}
-				while(!content.isEmpty()){
-					if(content.startsWith("http://")){
-						String propertyScore = resultSheet.getCellAt("C" + row).getTextValue();
-						questionnaireRating.store(id, content, propertyScore, rows);
+				Long id = -1l;
+				String textValue = resultSheet.getCellAt("A" + row).getTextValue();
+				if(!textValue.isEmpty()) id = Long.parseLong(textValue);
+				
+				if(id > 0){
+					String content = resultSheet.getCellAt("A" + row).getTextValue();
+					while(!content.equals("DBPEDIA PROPERTY")){
+						row++;
+						content = resultSheet.getCellAt("A" + row).getTextValue();
 					}
-					row++;
-					content = resultSheet.getCellAt("A" + row).getTextValue();
+					while(!content.isEmpty()){
+						if(content.startsWith("http://")){
+							String propertyScore = resultSheet.getCellAt("C" + row).getTextValue();
+							questionnaireRating.store(id, content, propertyScore, rows);
+						}
+						row++;
+						content = resultSheet.getCellAt("A" + row).getTextValue();
+					}
 				}
 			}
 			catch(Exception e){
-				//e.printStackTrace();
+				e.printStackTrace();
 			}
 		}
 		return rows;
