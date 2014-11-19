1c64993c0fea2269f2ae7b1e4a434c90b3da4256
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-19 09:56:32 -0600

    documentation cleanup

--- labelling/core/it/disco/unimib/labeller/tools/RunEvaluation.java
+++ labelling/core/it/disco/unimib/labeller/tools/RunEvaluation.java
@@ -10,7 +10,7 @@ public class RunEvaluation {
 
 	public static void main(String[] args) throws Exception {
 		Command command = new Command().withArgument("kb", "the knowledge base to use, namely dbpedia, dbpedia-with-labels, yago1, yago1-simple")
-					 .withArgument("algorithm", "the algorithm to use, namely mh, mhw, mhsw, mhwv, mhwcv, ml")
+					 .withArgument("algorithm", "the algorithm to use, namely mh, mhw, ml")
 					 .withArgument("occurrences", "the function applied to count each occurrence, namely simple, contextualized")
 					 .withArgument("summary", "the format of the results, namely questionnaire, trec")
 					 .withArgument("context", "context matching, namely no, partial, complete")
