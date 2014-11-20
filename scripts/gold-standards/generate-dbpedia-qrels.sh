4cfbc6f7d37d708de924ac318404cc7cbe51e661
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-20 11:48:37 -0600

    movings

--- scripts/gold-standards/generate-dbpedia-qrels.sh
+++ scripts/gold-standards/generate-dbpedia-qrels.sh
@@ -7,5 +7,5 @@ cd $root
 ../build.sh
 cd $root/../../labelling
 java -cp .:'labelling.jar' it.disco.unimib.labeller.tools.DBPediaQrels ../evaluation/gold-standards/questionnaires/questionnaire-ALL.ods ../evaluation/gold-standards/dbpedia-enhanced.qrels
-cd ..
+cd ../scripts/gold-standards
 ./validate.sh ../evaluation/gold-standards/dbpedia-enhanced.qrels ../evaluation/gold-standards/dbpedia-enhanced
--- validate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-relative_path=`dirname $0`
-root=`cd $relative_path;pwd`
-
-cd $root/labelling
-java -cp .:'labelling.jar' it.disco.unimib.labeller.tools.ValidateGoldStandard $1 $2
