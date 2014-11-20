9b4936b1605196d56ee053a58bcf34c446645620
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-20 11:38:09 -0600

    keep moving

---
+++ scripts/gold-standards/split-questionnaire.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+relative_path=`dirname $0`
+root=`cd $relative_path;pwd`
+
+cd $root
+../build.sh
+cd $root/../../labelling
+java -cp .:'labelling.jar' it.disco.unimib.labeller.tools.SplitQuestionnaire ../evaluation/gold-standards/questionnaires/questionnaire.ods ../evaluation/gold-standards/questionnaires/splitted-questionnaire/questionnaire-part
+
