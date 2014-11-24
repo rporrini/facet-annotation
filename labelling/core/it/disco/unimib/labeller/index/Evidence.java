4436a42d6594c65c72e8ebae0df015e42f3c37b1
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-24 13:07:42 -0600

    simplification of the implementation of the indexer

--- labelling/core/it/disco/unimib/labeller/index/Evidence.java
+++ labelling/core/it/disco/unimib/labeller/index/Evidence.java
@@ -24,30 +24,17 @@ public class Evidence implements TripleStore{
 	}
 	
 	public Evidence add(NTriple triple) throws Exception {
-		openWriter().addDocument(toDocument(triple));
-		return this;
-	}
-	
-	public Evidence closeWriter() throws Exception {
-		openWriter().close();
-		return this;
-	}
-	
-	private Document toDocument(NTriple triple) throws Exception {
 		Document document = new Document();
 		
 		document.add(new Field(indexFields.property(), triple.predicate().uri(), TextField.TYPE_STORED));
-		
 		String value = triple.object().contains("http://") ? "" : triple.object();
 		for(CandidateResource label : this.labels.get(triple.object(), "any")){
 			value += " " + label.value();
 		}		
 		document.add(new Field(indexFields.literal(), value, TextField.TYPE_STORED));
-		
 		for(CandidateResource type : this.types.get(triple.object(), "any")){
 			document.add(new Field(indexFields.objectType(), type.value(), TextField.TYPE_STORED));
 		}
-		
 		String context = "";
 		for(CandidateResource type : this.types.get(triple.subject(), "any")){
 			for(CandidateResource label : this.labels.get(type.value(), "any")){
@@ -56,10 +43,16 @@ public class Evidence implements TripleStore{
 			document.add(new Field(indexFields.subjectType(), type.value(), TextField.TYPE_STORED));
 		}
 		document.add(new Field(indexFields.context(), context, TextField.TYPE_STORED));
-		
 		document.add(new Field(indexFields.namespace(), triple.predicate().namespace(), TextField.TYPE_STORED));
 		document.add(new Field(indexFields.label(), triple.predicate().label(), TextField.TYPE_STORED));
-		return document;
+		
+		openWriter().addDocument(document);
+		return this;
+	}
+	
+	public Evidence closeWriter() throws Exception {
+		openWriter().close();
+		return this;
 	}
 	
 	private synchronized IndexWriter openWriter() throws Exception{
