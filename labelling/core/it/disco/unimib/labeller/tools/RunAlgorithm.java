2dcc6b0c5e5f14271f00c7d942ecc9e097474dc2
rporrini <riccardo.porrini@disco.unimib.it>
2014-11-30 13:21:08 -0600

    results analysis
    Conflicts:
    	labelling/core/it/disco/unimib/labeller/tools/RunAlgorithm.java

--- labelling/core/it/disco/unimib/labeller/tools/RunAlgorithm.java
+++ labelling/core/it/disco/unimib/labeller/tools/RunAlgorithm.java
@@ -8,14 +8,6 @@ public class RunAlgorithm {
 
 	public static void main(String[] args) throws Exception {
 		
-//		1668711967,
-//		1689442184,
-//		1744816435,
-//		1802054300,
-//		2021450258,
-//		2125380335,
-//		753388668
-		
 		CommandLineBenchmarkSimulation simulation = new CommandLineBenchmarkSimulation()
 					.onDBPedia()
 					.weightedFrequency()
