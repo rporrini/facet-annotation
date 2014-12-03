92952b90163aa6988a47e6aefe5cfbbfafda431c
rporrini <riccardo.porrini@disco.unimib.it>
2014-12-03 16:32:05 -0600

    simplified the specifity

--- labelling/core/it/disco/unimib/labeller/predicates/LogarithmicPredicateSpecificy.java
+++ labelling/core/it/disco/unimib/labeller/predicates/LogarithmicPredicateSpecificy.java
@@ -1,22 +1,17 @@
 package it.disco.unimib.labeller.predicates;
 
-import it.disco.unimib.labeller.index.AllValues;
 import it.disco.unimib.labeller.index.Index;
-import it.disco.unimib.labeller.index.NoContext;
-import it.disco.unimib.labeller.index.PartialContext;
 
 public class LogarithmicPredicateSpecificy implements Specificity{
 	
-	private Index index;
+	private Specificity specificity;
 	
 	public LogarithmicPredicateSpecificy(Index index) {
-		this.index = index;
+		this.specificity = new SimplePredicateSpeficity(index);
 	}
 	
 	@Override
-	public double of(String predicate, String context) throws Exception{
-		double frequencyOfPredicateAndContext = index.countPredicatesInContext(predicate, context, new PartialContext(new AllValues()));
-		double frequencyOfPredicate = index.countPredicatesInContext(predicate, context, new NoContext(new AllValues()));;
-		return Math.log((frequencyOfPredicateAndContext/frequencyOfPredicate) + 1.1);
+	public double of(String predicate, String domain) throws Exception{
+		return Math.log(specificity.of(predicate, domain) + 1.1);
 	}
 }
--- labelling/core/it/disco/unimib/labeller/predicates/SimplePredicateSpeficity.java
+++ labelling/core/it/disco/unimib/labeller/predicates/SimplePredicateSpeficity.java
@@ -3,22 +3,20 @@ package it.disco.unimib.labeller.predicates;
 import it.disco.unimib.labeller.index.AllValues;
 import it.disco.unimib.labeller.index.Index;
 import it.disco.unimib.labeller.index.NoContext;
-import it.disco.unimib.labeller.index.TripleSelectionCriterion;
+import it.disco.unimib.labeller.index.PartialContext;
 
 public class SimplePredicateSpeficity implements Specificity{
 
 	private Index index;
-	private TripleSelectionCriterion selectionCriterion;
 
-	public SimplePredicateSpeficity(Index index, TripleSelectionCriterion selectionCriterion) {
+	public SimplePredicateSpeficity(Index index) {
 		this.index = index;
-		this.selectionCriterion = selectionCriterion;
 	}
 	
 	@Override
 	public double of(String predicate, String domain) throws Exception {
-		double frequencyOfPredicateAndContext = index.countPredicatesInContext(predicate, domain, selectionCriterion);
+		double frequencyOfPredicateInDomain = index.countPredicatesInContext(predicate, domain, new PartialContext(new AllValues()));
 		double frequencyOfPredicate = index.countPredicatesInContext(predicate, domain, new NoContext(new AllValues()));;
-		return frequencyOfPredicateAndContext/frequencyOfPredicate;
+		return frequencyOfPredicateInDomain / frequencyOfPredicate;
 	}
 }
