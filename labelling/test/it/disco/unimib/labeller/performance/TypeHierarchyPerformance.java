790daa2c1ded9b9afaf12a8b753b352943a71868
rporrini <riccardo.porrini@disco.unimib.it>
2014-12-05 11:30:01 -0600

    performance per il calcolo della scaledDepth

---
+++ labelling/test/it/disco/unimib/labeller/performance/TypeHierarchyPerformance.java
@@ -0,0 +1,35 @@
+package it.disco.unimib.labeller.performance;
+
+import it.disco.unimib.labeller.index.InputFile;
+import it.disco.unimib.labeller.index.TypeHierarchy;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+
+@BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 0, concurrency=2)
+public class TypeHierarchyPerformance extends AbstractBenchmark{
+
+	private static TypeHierarchy hierarchy;
+
+	@BeforeClass
+	public static void annotateAnotherFacetInOrderToEnsureThatLuceneDoesNotCacheResults() throws Exception{
+		hierarchy = new TypeHierarchy(
+										new InputFile(new File("../evaluation/dbpedia-type-tree/type-tree.nt")),
+										new InputFile(new File("../evaluation/dbpedia-category-tree/category-tree.nt")));
+	}
+	
+	@Test
+	public void scaledDepthOfAType() {
+		hierarchy.of("http://dbpedia.org/ontology/MusicalArtist").scaledDepth();
+	}
+	
+	@Test
+	public void scaledDepthOfACategory() {
+		hierarchy.of("http://dbpedia.org/resource/Category:World_War_II").scaledDepth();
+	}
+}
