b18c151b60535bc5d4f612dc35695979ffe05962
rporrini <riccardo.porrini@disco.unimib.it>
2015-01-07 09:59:19 +0100

    added the deletions and the merging of the branches

--- scripts/repository/branch.sh
+++ scripts/repository/branch.sh
@@ -8,19 +8,33 @@ relative_path=`dirname $0`
 root=`cd $relative_path;pwd`
 cd $root/../..
 
+if [ $(git status -s | wc -l) != 0 ] 
+then
+	echo 'aborting due to local modifications'
+	git status
+	exit 1
+fi
+
+git pull
+
 case $action in
 	new )
 		from_branch=$2
 		new_branch=$3
-		if [ $(git status -s | wc -l) != 0 ] 
-		then
-			echo 'aborting due to local modifications'
-			git status
-			exit 1
-		fi
-		git pull
 		git checkout $from_branch
 		git checkout -b $new_branch
 		git push --set-upstream origin $new_branch
+		;;
+	delete )
+		branch=$2
+		git branch -D $branch
+		git push origin --delete $branch
+		;;
+	merge )
+		branch=$2
+		with_branch=$3
+		git checkout $with_branch
+		git merge --no-ff $branch
+		git push
 		;;
 esac
