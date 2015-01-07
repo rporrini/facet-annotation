5f5396ee6b1f8af20f1ef4fab7f5e995375d45e4
rporrini <riccardo.porrini@disco.unimib.it>
2015-01-07 09:48:41 +0100

    script to create a new branch

---
+++ scripts/repository/branch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+action=$1
+
+set -e
+
+relative_path=`dirname $0`
+root=`cd $relative_path;pwd`
+cd $root/../..
+
+case $action in
+	new )
+		from_branch=$2
+		new_branch=$3
+		if [ $(git status -s | wc -l) != 0 ] 
+		then
+			echo 'aborting due to local modifications'
+			git status
+			exit 1
+		fi
+		git pull
+		git checkout $from_branch
+		git checkout -b $new_branch
+		git pull
+		;;
+esac
