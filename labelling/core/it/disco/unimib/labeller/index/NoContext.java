8a554b10ca28afccdd26bde4891f630115d6c182
rporrini <riccardo.porrini@disco.unimib.it>
2015-01-07 13:09:39 +0100

    deleted unuseful code

--- labelling/core/it/disco/unimib/labeller/index/NoContext.java
+++ labelling/core/it/disco/unimib/labeller/index/NoContext.java
@@ -1,11 +1,6 @@
 package it.disco.unimib.labeller.index;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
-import org.apache.lucene.queryparser.flexible.standard.config.StandardQueryConfigHandler;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery;
 
 public class NoContext implements TripleSelectionCriterion{
@@ -17,11 +12,6 @@ public class NoContext implements TripleSelectionCriterion{
 	}
 
 	public BooleanQuery asQuery(String value, String context, String literalField, String contextField, String namespaceField, Analyzer analyzer) throws Exception {
-		BooleanQuery query = allValues.createQuery(value, literalField, analyzer);
-		
-		StandardQueryParser standardQueryParser = new StandardQueryParser(analyzer);
-		standardQueryParser.setDefaultOperator(StandardQueryConfigHandler.Operator.OR);
-		query.clauses().add(new BooleanClause(standardQueryParser.parse(QueryParser.escape(context), contextField), Occur.SHOULD));
-		return query;
+		return allValues.createQuery(value, literalField, analyzer);
 	}
 }
