6f7199ed793296f6ddbdda9f99e6ff867c95b28c
rporrini <riccardo.porrini@disco.unimib.it>
2015-02-14 17:14:42 +0100

    now also the regression tests can be run just selectively

--- build-and-test.sh
+++ build-and-test.sh
@@ -30,7 +30,16 @@ signal "Done"
 
 if [[ $1 != -skip-regression-tests ]] && [[ $2 != -skip-regression-tests ]]
 then
-	signal "Running Regression Tests"
-	java -Xms256m -Xmx3000m -cp .:'labelling.jar' org.junit.runner.JUnitCore it.disco.unimib.labeller.regression.RegressionTests
-	signal "Done"
+	if [[ $kb == dbpedia ]] || [[ $kb == '' ]]
+	then
+		signal "Running Regression Tests for DBPedia"
+		java -Xms256m -Xmx3000m -cp .:'labelling.jar' org.junit.runner.JUnitCore it.disco.unimib.labeller.regression.DBPediaRegressionTest
+		signal "Done"
+	fi
+	if [[ $kb == yago1 ]] || [[ $kb == '' ]]
+	then
+		signal "Running Regression Tests for YAGO1"
+		java -Xms256m -Xmx3000m -cp .:'labelling.jar' org.junit.runner.JUnitCore it.disco.unimib.labeller.regression.YAGO1RegressionTest
+		signal "Done"
+	fi
 fi
