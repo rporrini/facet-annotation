d1c6810813178d133cf091524a1af68fa0b400d8
rporrini <riccardo.porrini@disco.unimib.it>
2015-07-30 14:42:40 +0200

    small refactoring

--- labelling/core/it/disco/unimib/labeller/properties/TypeDistribution.java
+++ labelling/core/it/disco/unimib/labeller/properties/TypeDistribution.java
@@ -5,50 +5,61 @@ import java.util.Set;
 
 public class TypeDistribution {
 
-	private HashMap<String, Double> typeFrequencies;
-	private HashMap<String, Double> propertyFrequenciesForTypes;
+	private HashMap<String, Double[]> frequencies;
 
 	public TypeDistribution() {
-		this.typeFrequencies = new HashMap<String, Double>();
-		this.propertyFrequenciesForTypes = new HashMap<String, Double>();
+		this.frequencies = new HashMap<String, Double[]>();
 	}
 
 	public void trackTypeOccurrence(String type, String occurrence) {
-		trackIn(type, occurrence, typeFrequencies);
-	}
-	
-	public void trackPropertyOccurrenceForType(String type, String occurrence) {
-		trackIn(type, occurrence, propertyFrequenciesForTypes);
+		trackAs(type, occurrence, typeOccurrence()); 
 	}
 
-	private void trackIn(String type, String occurrence, HashMap<String, Double> frequencies) {
-		if (!frequencies.containsKey(type)) frequencies.put(type, 0.0);
-		frequencies.put(type, frequencies.get(type) + Double.parseDouble(occurrence));
+	public void trackPropertyOccurrenceForType(String type, String occurrence) {
+		trackAs(type, occurrence, propertyOccurrence());
 	}
-	
+
 	public int size() {
-		return typeFrequencies.size();
+		return all().size();
 	}
 
 	public HashMap<String, Double> getTypeFrequencies() {
-		return typeFrequencies;
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		for(String type : all()){
+			result.put(type, get(type, typeOccurrence()));
+		}
+		return result;
 	}
 
 	public Double typeOccurrence(String type) {
-		return getOrDefault(type, typeFrequencies);
-	}
-
-	public Double propertyOccurrenceForType(String type) {
-		return getOrDefault(type, propertyFrequenciesForTypes);
+		return get(type, typeOccurrence());
 	}
 	
-	private Double getOrDefault(String type, HashMap<String, Double> frequencies) {
-		Double frequency = frequencies.get(type);
-		if (frequency == null) frequency = 0.0;
-		return frequency;
+	public Double propertyOccurrenceForType(String type) {
+		return get(type, propertyOccurrence());
 	}
 
 	public Set<String> all() {
-		return typeFrequencies.keySet();
+		return frequencies.keySet();
+	}
+	
+	private int typeOccurrence() {
+		return 0;
+	}
+	
+	private int propertyOccurrence() {
+		return 1;
+	}
+	
+	private void trackAs(String type, String occurrence, int index) {
+		if (!frequencies.containsKey(type)) frequencies.put(type, new Double[]{0.0, 0.0});
+		frequencies.get(type)[index] += Double.parseDouble(occurrence);
+	}
+	
+	private Double get(String type, int index) {
+		Double[] frequencies = this.frequencies.get(type);
+		double frequency = 0.0;
+		if (frequencies != null) frequency = frequencies[index];
+		return frequency;
 	}
 }
