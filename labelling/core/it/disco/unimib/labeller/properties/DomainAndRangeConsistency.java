cc528a4aa07013e087bd262c4e27fc816fd376c4
rporrini <riccardo.porrini@disco.unimib.it>
2015-09-01 12:00:10 +0200

    yesss!!! better results

--- labelling/core/it/disco/unimib/labeller/properties/DomainAndRangeConsistency.java
+++ labelling/core/it/disco/unimib/labeller/properties/DomainAndRangeConsistency.java
@@ -41,7 +41,7 @@ public class DomainAndRangeConsistency implements AnnotationAlgorithm{
 				frequencyOverValues += score;
 			}
 			
-			WeightedJaccardSimilarity similarity = new WeightedJaccardSimilarity();
+			JaccardSimilarity similarity = new JaccardSimilarity();
 			
 			TypeDistribution domains = statistics.domainsOf(property);
 			TypeDistribution domainSummaries = this.domainSummaries.of(property);
@@ -56,7 +56,7 @@ public class DomainAndRangeConsistency implements AnnotationAlgorithm{
 			
 			double domainSimilarity = 1.0 + Math.log(similarity.between(domains, domainSummaries) + 1.000000001);
 			double rangeSimilarity = 1.0 + Math.log(similarity.between(ranges, rangeSummaries) + 1.000000001);
-			double smoothedWFreq = 1.0 / (1.0 + (Math.exp(((double)distribution.values().size() / 2) - frequencyOverValues)));
+			double smoothedWFreq = Math.log(frequencyOverValues / (double)distribution.values().size() + 1.000000001);
 			double coverage = covered / (double)distribution.values().size();
 			
 			CandidateProperty resource = new CandidateProperty(property);
